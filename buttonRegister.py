bb874f62f1f86f5b68c6373f4be1dd81a987b89c
BarnacleJones <benmjones@protonmail.com>
2023-07-02 20:25:12 +1200

    rainbow button

--- buttonRegister.py
+++ buttonRegister.py
@@ -2,9 +2,10 @@
 from gpiozero import Button
 import RPi.GPIO as GPIO
 from time import sleep
+import random
 from ShiftRegister import ShiftRegister
 GPIO.setmode(GPIO.BCM)
-	#globals
+#globals
 button = Button(12)
 button_flag = False
 
@@ -15,9 +16,21 @@ def button_pressed_event():
 	else:
 		shift_register.clear()
 	button_flag = not button_flag
+	
+def button_held_event():
+	while button.is_pressed:
+		arr = []
+		for x in range(8):
+			pattern = str(random.randint(0,1))
+			arr.append(pattern)
+		combo = ''.join(arr)
+		shift_register.write(combo)
+		sleep(0.06)
+	shift_register.write("00000000")
 
 shift_register = ShiftRegister(data_pin=16, latch_pin=20, clock_pin=21)
-button.when_pressed = button_pressed_event
+#button.when_pressed = button_pressed_event
+button.when_held = button_held_event
 try:
 	while True:
 		sleep(0.1)
@@ -26,4 +39,5 @@ except KeyboardInterrupt:
 	pass
 
 finally:
+	shift_register.clear()
 	GPIO.cleanup()
