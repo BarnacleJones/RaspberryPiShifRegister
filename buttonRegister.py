7fc6a1f77eba1213e3692ae450033ad14eea55f7
Banjo <benmjones@protonmail.com>
2023-07-02 19:29:28 +1200

    fix button

--- buttonRegister.py
+++ buttonRegister.py
@@ -1,27 +1,21 @@
 #script to change shift register state on button press
-from tkinter import BUTT
 from gpiozero import Button
+import RPi.GPIO as GPIO
 from time import sleep
 from ShiftRegister import ShiftRegister
-
-def main(): 
+GPIO.setmode(GPIO.BCM)
 	#globals
+button = Button(12)
+button_flag = False
+
+def button_pressed_event():
 	shift_register = ShiftRegister(data_pin=16, latch_pin=20, clock_pin=21)
-	button = Button(12)
-	button_flag = False
+	global button_flag
+	if button_flag:
+		shift_register.write("11111111")
+	else:
+		shift_register.clear()
+	button_flag = not button_flag
 
-	def button_pressed_event():
-		global button_flag
-		if button_flag:
-			shift_register.write("11111111")
-		else:
-			shift_register.clear()
-		button_flag = not button_flag
-
-	button.when_pressed = button_pressed_event
-	pass
-
-main()
-
-
+button.when_pressed = button_pressed_event
 
